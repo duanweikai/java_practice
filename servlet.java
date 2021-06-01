c350478fba01502076d7681ca91d0e7c8b0b810c
duanweikai <906287946@qq.com>
2021-06-01 19:08:24 +0800

    mYservlet.java

---
+++ servlet.java
@@ -0,0 +1,20 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/hello")
+public class servlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        resp.setCharacterEncoding("utf-8");
+        PrintWriter writer=resp.getWriter();
+        writer.println("<h1>你好世界</h1>");
+
+
+    }
+}
