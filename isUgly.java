697eeeb22f713985a3c974e416f85a622c385503
duanweikai <906287946@qq.com>
2021-07-12 18:59:27 +0800

    丑数

---
+++ isUgly.java
@@ -0,0 +1,33 @@
+public class isUgly {
+    public static boolean ugly(int n){
+        if(n==1){
+            return true;
+        }
+        if(n<=0){
+            return false;
+        }
+        while (n%2==0){
+            n=n/2;
+            if(n==1){
+                return true;
+            }
+        }
+        while (n%3==0){
+            n=n/3;
+            if(n==1){
+                return true;
+            }
+        }
+        while (n%5==0){
+            n=n/5;
+            if(n==1){
+                return true;
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+            System.out.println(ugly(100));
+
+    }
+}
